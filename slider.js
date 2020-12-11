3168b27c3f68c93c8e45516588e308331a5c9b6e
Roman Pesnya <ipromanone@gmail.com>
2020-12-11 10:27:20 +0300

    Create slider.js

---
+++ slider.js
@@ -0,0 +1,50 @@
+let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if(n > slides.length) {
+            slideIndex = 1;
+        }
+        if(n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) { // Тоже самое но другой вариант
+        //     slides.stile.display = 'none';
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function pluseSlides(n) {
+        showSlides(slideIndex += n); 
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        pluseSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+pluseSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for(let i = 0; i < dots.length + 1; i++) {
+            if(event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i); 
+            }
+        }
+    });
